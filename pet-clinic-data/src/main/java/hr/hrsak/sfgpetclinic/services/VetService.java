438b965dba9c27cbfbce48d1b2abb77ab2c8354d
fhrsak <hrsak.filip@gmail.com>
2022-07-05 23:43:42 +0200

    Adding Services, Closes #5.

---
+++ pet-clinic-data/src/main/java/hr/hrsak/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package hr.hrsak.sfgpetclinic.services;
+
+import hr.hrsak.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
