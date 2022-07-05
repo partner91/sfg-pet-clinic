bdf7920765fa7e191747c40f40c0e197b0b2f0a6
fhrsak <hrsak.filip@gmail.com>
2022-07-05 23:51:02 +0200

    Adding base entity, Closes #11.

---
+++ pet-clinic-data/src/main/java/hr/hrsak/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,15 @@
+package hr.hrsak.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+
+}
