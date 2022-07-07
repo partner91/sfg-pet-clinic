d086e3bd7ffdd1c6974503be3d4ec6c12d516c3c
fhrsak <hrsak.filip@gmail.com>
2022-07-07 15:50:58 +0200

    Index Page controller added. Closes #8.

---
+++ pet-clinic-web/src/main/java/hr/hrsak/sfgpetclinic/controllers/indexController.java
@@ -0,0 +1,14 @@
+package hr.hrsak.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "index.html"})
+    public String getIndex(){
+
+        return "index";
+    }
+}
