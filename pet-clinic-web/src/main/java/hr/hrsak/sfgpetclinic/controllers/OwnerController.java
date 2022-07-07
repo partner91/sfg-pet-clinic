8c68da1d261e2a9391c2a512dccb25c89fef4d40
fhrsak <hrsak.filip@gmail.com>
2022-07-07 16:10:09 +0200

    adding owner controller and index page. Closes #14

---
+++ pet-clinic-web/src/main/java/hr/hrsak/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package hr.hrsak.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({ "", "/",  "index.html", "index"})
+    public String listOWners(){
+        return "owners/index";
+    }
+}
