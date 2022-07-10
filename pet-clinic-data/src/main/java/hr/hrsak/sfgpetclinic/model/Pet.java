3f0b234ed230ff37909353bb1519f4d5aae4f69e
fhrsak <hrsak.filip@gmail.com>
2022-07-10 11:35:19 +0200

    Added SpecialtiesService Closes #31

--- pet-clinic-data/src/main/java/hr/hrsak/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/hr/hrsak/sfgpetclinic/model/Pet.java
@@ -1,5 +1,6 @@
 package hr.hrsak.sfgpetclinic.model;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class Pet extends BaseEntity{
+    private String name;
     private PetType petType;
     private Owner owner;
     private LocalDate birthDay;
---
+++ pet-clinic-data/src/main/java/hr/hrsak/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package hr.hrsak.sfgpetclinic.services;
+
+import hr.hrsak.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
